ef73d0b5bfae72d780ef55ba01298e7731b7b364
aloy <zerolimits45@gmail.com>
2021-02-23 16:49:00 +0800

    Updated again

--- 05(AloysiusLIm)_ASP Project/grp5_ASP_project.py
+++ 05(AloysiusLIm)_ASP Project/grp5_ASP_project.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-class Asia5():
+class Asia5:
 
     imva = pd.read_excel('IMVA.xls')  # import xls file that is in the same folder
 
@@ -38,50 +38,8 @@ class Asia5():
 
         all_SUM_of_asia5.append(sum(sum_of_each_col))
         all_COUNTRY_of_asia5.append(asia5[col_id].name)
-        #print("this is the sum of each column: " + str(sum(all_SUM_of_asia5)))
-        #print("---------------")
-
-        sum_2011 = sum(sum_of_each_col[0:11])
-        sum_2012 = sum(sum_of_each_col[12:23])
-        sum_2013 = sum(sum_of_each_col[34:45])
-        sum_2014 = sum(sum_of_each_col[34:45])
-        sum_2015 = sum(sum_of_each_col[45:56])
-        sum_2016 = sum(sum_of_each_col[56:67])
-        sum_2017 = sum(sum_of_each_col[67:78])
-        sum_2018 = sum(sum_of_each_col[78:89])
-        sum_2019 = sum(sum_of_each_col[89:100])
-        sum_2020 = sum(sum_of_each_col[100:111])
-
-        print("Year 2011")
-        print(sum_of_each_col[0:11])
-        print("------")
-        print("Year 2012")
-        print(sum_of_each_col[12:23])
-        print("------")
-        print("Year 2013")
-        print(sum_of_each_col[34:45])
-        print("------")
-        print("Year 2014")
-        print(sum_of_each_col[34:45])
-        print("------")
-        print("Year 2015")
-        print(sum_of_each_col[45:56])
-        print("------")
-        print("Year 2016")
-        print(sum_of_each_col[56:67])
-        print("------")
-        print("Year 2017")
-        print(sum_of_each_col[67:78])
-        print("------")
-        print("Year 2018")
-        print(sum_of_each_col[78:89])
-        print("------")
-        print("Year 2019")
-        print(sum_of_each_col[89:100])
-        print("------")
-        print("Year 2020")
-        print(sum_of_each_col[100:111])
-
+        print("this is the sum of each column: " + str(sum(all_SUM_of_asia5)))
+        print("---------------")
 
     final_dataframe_of_asia5 = pd.DataFrame()
 
@@ -90,19 +48,22 @@ class Asia5():
     final_dataframe_of_asia5["country"] = all_COUNTRY_of_asia5
     final_dataframe_of_asia5["values"] = all_SUM_of_asia5
     final_dataframe_of_asia5.sort_values(by=['values'], inplace=True, ascending=False)
-
     # Bar chart for the top 3 countries
     final_dataframe_of_asia5.head(3).plot.bar(x="country", y="values", rot=70,
                                               title="Top 3 COUNTRIES from period 2011 - 2020",
-                                              ylabel="Number of travelers [in millions]")
-    #print(final_dataframe_of_asia5.head(3)
-    sum_of_top3 = 26713289 + 23203897 + 11005817
-    mean_of_top3 = sum_of_top3/3
+
+                                            ylabel="Number of travelers [in millions]")
+    plt.savefig("top3Countries.png")
+    plt.show()
+
+    sum_of_top3 = sum(final_dataframe_of_asia5['values'].head(3).to_list())
+    mean_of_top3 = final_dataframe_of_asia5.head(3).mean()
+
     print("")
     print("The mean value for the top 3 countries is " + str((round((mean_of_top3), 2))))
-    print("The total no. of visitors for the top 3 countries is " + str((sum_of_top3)))
+    print("The total no. of visitors for the top 3 countries is " + str(sum_of_top3))
 
-    plt.savefig("top3Countries.png")
+
 
     # Bar chart for all the countries
     final_dataframe_of_asia5.plot.bar(x="country", y="values", rot=70,
@@ -110,6 +71,8 @@ class Asia5():
                                       figsize=(10, 10),
                                       ylabel="Number of travelers [in millions]", )
     plt.savefig("allCountries.png")
+    plt.show()
+
 
 
 
